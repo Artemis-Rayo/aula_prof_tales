dc881aa5da316c5a0530dbbb9d6b20b1b495f7bf
Rayanne Marques <rayannea.marques18@gmail.com>
2025-04-23 18:45:07 -0400

    "adcionando navigation'

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/screens/Home'
+// In App.js in a new project
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Home></Home>
-    </View>
-  );
-}
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import EditarTarefa from './src/screens/EditTask';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const RootStack = createNativeStackNavigator({
+  
+  screens: {
+    Home: Home,
+    EditarTarefa: EditarTarefa 
+    
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
