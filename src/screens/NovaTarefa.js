ee5b206b6f08c0f3cadc6d12f89a00f5e064855b
Rayanne Marques <rayannea.marques18@gmail.com>
2025-04-28 21:07:24 -0400

    tela de adicionar tarefa

--- src/screens/NovaTarefa.js
+++ src/screens/NovaTarefa.js
@@ -1,21 +1,21 @@
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 export default function Novatarefa(){
-    const [text, onChangeText] = React.useState('');
-
     return(
         <View style={styles.container}>
             <View style={styles.cabecalho}>
                 <Text style={styles.titulo}>ADCIONAR TAREFA</Text>
             </View>
-            <View>
-                <Text>Nome da tarefa</Text>
+            <View style={styles.body}>
+                <Text style={styles.texto}>Nome da tarefa: </Text>
+                    <TextInput style={styles.TextInput}/>
+
+                <Text style={styles.categoria}>Categoria da Tarefa: </Text>
+
+                <Text style={styles.texto1}>Descrição da Tarefa:</Text>
+                    <TextInput style={styles.textoTarefa}/>
             </View>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
-            />
+
         </View>
     )
 }
@@ -39,10 +39,22 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight: 'bold'
     },
-    input: {
-        height: 40,
-        margin: 12,
+    TextInput: {
         borderWidth: 1,
-        padding: 10
+        padding: 10,
+        borderColor: '#ccc',
+        backgroundColor: 'white',
+        borderRadius: 10
+    },
+    body: {
+        padding: 15,
+        flex: 1,
+    },
+    textoTarefa:{
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#ccc',
+        backgroundColor: 'white',
+        borderRadius: 10
     }
 });
